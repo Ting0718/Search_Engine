1692faae9b7f5d2c626a5dc6e8ef900f2e530c6c
Jason <jasoz10@uci.edu>
2020-02-23 14:52:36 -0800

    Create main.py
    Adjust

---
+++ main.py
@@ -0,0 +1,21 @@
+
+if __name__=="__main__":
+    print("starting")
+
+def parseFiles(filename:str):
+    ''' Reads through the corpus '''
+
+
+def writeFiles():
+    '''Writes parsed information onto the disk'''
+
+def mergeFiles():
+    ''' Merging files '''
+
+def tfidf():
+    ''' calculate the tf-idf '''
+
+
+
+
+
