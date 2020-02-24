019679015de92e2b846b35fc2dc23c7ac0743632
Scott Buchmiller <scottbuchmiller@gmail.com>
2020-02-23 17:06:03 -0800

    added html content parse and tokenize

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import nltk
 from nltk.stem import PorterStemmer
 import json
+from bs4 import BeautifulSoup
+import tokenizer
+
+blackList = ['[document]', 'noscript', 'head', 'header', 'html', 'meta', 'input', 'script', 'style', 'b', 'button']
 
 def readFiles(mypath:str):
     '''parsing through all the files'''
@@ -13,6 +17,20 @@ def readFiles(mypath:str):
 
 def parseFiles(filename:str):
     ''' Reads through the corpus '''
+    f = open(filename, 'r')
+    content = json.load(f)
+
+    url = content["url"]
+    html = content["content"]
+
+    output = " "
+    soup = BeautifulSoup(html, "lxml")
+    text = soup.find_all(text=True)
+    for t in text:
+        if t.parent.name not in blackList:
+            output += '{} '.format(t)
+    output = tokenizer.tokenize(output)
+    return output
 
 
 def writeFiles(inverted_index: dict, file: "JSON file"):
@@ -35,7 +53,9 @@ def porterstemer(s:str):
     return (porter.stem(s))
 
 if __name__=="__main__":
-    path = "/Users/jason/Desktop/ANALYST"
+    path = "ANALYST"
     files = readFiles(path)
+    for file in files:
+        print(parseFiles(file))
 
 
---
+++ tokenizer.py
@@ -0,0 +1,35 @@
+import re
+import sys
+from collections import defaultdict
+
+def tokenize(text: str) -> [str]:
+    """
+    Accepts a string representing a file path as input and returns a list of all lowercase, alphanumeric tokens that
+    are separated by spaces or non-alphanumeric characters in the original text.
+    if the file path does not exist returns an empty list
+    """
+    token_list = []
+    text = re.sub(r"[^a-zA-Z0-9]+"," ",text)
+    text = text.strip()
+    #text = re.sub(r"[\w]+"," ",text)
+    text = text.lower()
+    return text.split()
+
+def computeWordFrequencies(tokens:[str]) -> {str:int}:
+    """
+     converts a list of tokens into a dictionary that maps token:frequency
+    """
+    freq = defaultdict(int)
+    for t in tokens:
+        freq[t] += 1
+    return dict(freq)
+
+def print_tokens(tokens:{str:int}):
+    """
+    prints the frequency dictionary using the format <token> -> <frequency> from greatest frequency to lowest
+    """
+    for t,f in sorted(tokens.items(),key = lambda x: x[1],reverse = True):
+        print(str(t)+" -> "+str(f))
+
+
+
