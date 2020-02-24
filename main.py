53d7d1fd08c3fc7369a4b6f6021b10d1f7ad3c2f
Scott Buchmiller <scottbuchmiller@gmail.com>
2020-02-23 19:08:20 -0800

    added doc_id class to keep track of which doc maps to which id

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ import tokenizer
 
 blackList = ['[document]', 'noscript', 'head', 'header', 'html', 'meta', 'input', 'script', 'style', 'b', 'button']
 
+class DocID:
+    def __init__(self):
+        self.current_doc = 0
+        self.doc_ids = dict()
+
+    def add_to_docs(self,url: str):
+        '''Assigns a doc_id to a url and adds it to the doc_ids dictionary'''
+        self.doc_ids[self.current_doc] = url
+        self.current_doc += 1
+        return self.current_doc - 1
+
 def readFiles(mypath:str):
     '''parsing through all the files'''
     filepaths = []
@@ -39,7 +50,6 @@ def writeFiles(inverted_index: dict, filename: str):
     for k, v in inverted_index.items():
         f.write(k + " " + " ".join(map(str, sorted(v))) + "\n")
     f.close()
-    
 
 def mergeFiles():
     ''' Merging files '''
@@ -52,13 +62,16 @@ def porterstemer(s:str):
     porter = PorterStemmer()
     return (porter.stem(s))
 
+
+
 if __name__=="__main__":
-    # path = "ANALYST/www-db_ics_uci_edu"
-    # files = readFiles(path)
-    # a = []
-    d = {"apple": [2, 4], "aardvark": [2, 1, 7, 5]}
-    # for file in files:
-    #     a.append(parseFiles(file))
+    path = "ANALYST/www-db_ics_uci_edu"
+    files = readFiles(path)
+    d = DocID()
+    a = []
+    #d = {"apple": [2, 4], "aardvark": [2, 1, 7, 5]}
+    for file in files:
+        a.append(parseFiles(file))
     writeFiles(d,"test.txt")
 
 
