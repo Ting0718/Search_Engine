07743db9d73be5a2fd280550384cd3c2a8b7e03a
Shireen Hsu <shireen0718@gmail.com>
2020-03-05 20:16:18 -0800

    set up a website

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import os
 import time
 import json
 
+
 def porterstemmer(s: str):
     '''porter stemmer'''
     porter = PorterStemmer()
@@ -29,36 +30,38 @@ def translate_ids(id_dict:dict,id_list):
         ret.append(id_dict[str(id)])
     return ret
 
-if __name__ == "__main__":
-
+def search_result(queries:list):
     docIds = json.load(open("docID.json",'r'))
     split = ["9",'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     files = {}
     for x in split:
         files[x] = f"outputs/output{x}.txt"
 
-    queries = [porterstemmer(x) for x in input("Enter Search: ").split()]
+    queries = [porterstemmer(x) for x in queries.split()]
     start_time = time.time()
     q = sorted(queries)
     list_of_posting = []
     ''' Time could be improved if you don't open the file evertime, or open it once for each start letter.'''
-    fileStorage = []
     for query in q:
         with open(files[query[0]],'r') as f:
             for line in f:
                 if getToken(line) == query:
                     list_of_posting.append(SetOfDocId(line))
-    # f = open("output.txt",'r')
-    # index = 0
-    # for line in f:
-    #     if getToken(line) == q[index]:
-    #         list_of_posting.append(SetOfDocId(line))
-    #         index += 1
-    #         if(index >= len(q)):
-    #             break
-    #         stemmed = q[index]
-    # f.close()
+
     
     top_five = translate_ids(docIds,mergePostings(list_of_posting)[:5]) # return the first 5 URLst
-    print(top_five)
-    print("--- %s seconds ---" % (time.time() - start_time))
+    top_five.append(time.time() - start_time)
+    return(top_five)
+    
+
+
+# f = open("output.txt",'r')
+# index = 0
+# for line in f:
+#     if getToken(line) == q[index]:
+#         list_of_posting.append(SetOfDocId(line))
+#         index += 1
+#         if(index >= len(q)):
+#             break
+#         stemmed = q[index]
+# f.close()
