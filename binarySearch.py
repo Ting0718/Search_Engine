56ff737c71a2b72c8840aa5022762a00f90a31d7
Scott Buchmiller <scottbuchmiller@gmail.com>
2020-03-06 22:14:45 -0800

    fix binary search

--- binarySearch.py
+++ binarySearch.py
@@ -1,6 +1,6 @@
 import math
 
-def search(content:list,item, max = 26):
+def search(content:list,item, max_iterations = 26):
     ''' searches content, a list which is assumed to be sorted, for item. if item is not found after the max iterations
     it returns the location of the closest item to value, if it is found returns the location of item
     '''
@@ -8,17 +8,19 @@ def search(content:list,item, max = 26):
     left = 0
     right = len(content)-1
     mid = left + int((right - left) / 2)
-    while iterations <= max and content[mid] != item:
+    while iterations <= max_iterations and content[mid] != item:
+        if content[mid] == item:
+            return mid
         if left == right:
             return left
-        elif content[mid] < item:
-            left = mid+1
+        elif item < content[mid]:
+            right = mid
             mid = left+int((right-left)/2)
         else:
-            right = mid-1
+            left = mid
             mid = left + int((right - left) / 2)
         iterations += 1
-    if content[mid] == item:
-        return mid
-    return mid-1
+    if item < content[mid]:
+        return mid+1
+    return mid
 
